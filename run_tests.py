fddd932901db2bcd859e6f7024084b46df4c4032
Andrew High <andrewhigh4@hotmail.com>
2021-04-25 13:02:54 +0100

    whisky tests passing

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,6 @@
 import unittest
 from tests.distillery_test import TestDistillery
-# from tests.whisky_test import TestWhisky
+from tests.whisky_test import TestWhisky
 # from tests.user_test import TestUser
 # from tests.review_test import TestReview
 
