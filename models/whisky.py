d6c974923eb44602b2defa73126b8a45ffa48b47
Andrew High <andrewhigh4@hotmail.com>
2021-04-25 13:01:38 +0100

    whisky tests built

--- models/whisky.py
+++ models/whisky.py
@@ -1,5 +1,5 @@
 class Whisky:
-    def __init__(self, name, type, flavour_profile = [], id = None):
+    def __init__(self, name, type, flavour_profile = None, id = None):
         self.name = name
         self.type = type
         self.flavour_profile = flavour_profile
--- tests/whisky_test.py
+++ tests/whisky_test.py
@@ -1,4 +1,17 @@
 import unittest
 from models.whisky import Whisky
 
-# class TestWhisky(unittest.TestCase):
+class TestWhisky(unittest.TestCase):
+    def setUp(self):
+        self.whisky_1 = Whisky("Classic Laddie", "Single Malt", "Coastal")
+        self.whisky_2 = Whisky("Famous Grouse", "Blended Whisky")
+        self.whisky_3 = Whisky("Port Charlotte 10", "Single Malt", "Peated")
+
+    def test_whisky_has_name(self):
+        self.assertEqual("Famous Grouse", self.whisky_2.name)
+
+    def test_whisky_has_type(self):
+        self.assertEqual("Single Malt", self.whisky_3.type)
+
+    def test_whisky_has_flavour_profile(self):
+        self.assertEqual("Coastal", self.whisky_1.flavour_profile)
